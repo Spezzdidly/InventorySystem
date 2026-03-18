f2b756e7786b09f5ac79c6178f85e4628693985c
Spezzdidly <spencer.johns@mines.sdsmt.edu>
2026-03-17 18:27:59 -0600

    Added proper dynamic allocation for the inventorySlot struct. Added inventory grid deallocation in the deconstructor.

--- inventory.h
+++ inventory.h
@@ -13,29 +13,37 @@ class Inventory {
     private:
         // Plan on this being a 4 or 5 row and an incomplete number of rows
         // for inventory upgrades? All still square or rectangle shaped
+        int row;
+        int col;
         inventorySlot** grid;
         
 };
 
 Inventory::Inventory() {
-    int r = 3;
-    int c = 8;
+    this->row = 3;
+    this->col = 8;
 
     // Allocate grid array
-    this->grid = new int*[r];
-    for (int i = 0; i < r; i++) {
-        this->grid[i] = new int[c];
+    this->grid = new inventorySlot*[this->row];
+    for (int i = 0; i < this->row; i++) {
+        this->grid[i] = new inventorySlot[this->col];
     }
 
     // Initialize to all zeros
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            this->grid[i][j] = 0;
+    for (int i = 0; i < this->row; i++) {
+        for (int j = 0; j < this->col; j++) {
+            this->grid[i][j] = new inventorySlot();
         }
     }
 }
 
-Inventory::~Inventory() {};
+Inventory::~Inventory() {
+    for (int i =0; i < this->row; i++) {
+        delete[] this->grid[i];
+    }
+
+    delete[] this->grid;
+};
 
 struct inventorySlot {
     int itemID = 0;
