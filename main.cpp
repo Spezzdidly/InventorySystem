c701196ce63c89bdfa8de344bef67731fee3f2fd
Spezzdidly <s.johns3404@gmail.com>
2026-03-20 20:39:49 -0600

    Changed userInput to use std::getline rather than whatever I thought I was trying to do before.

--- main.cpp
+++ main.cpp
@@ -4,35 +4,35 @@
 
 int main() {
     Player *player = nullptr;
-    std::string userName = "";
-    int userInput = 0;
+    std::string line;
+    int userInput;
+    std::string userName;
 
     // Main menu loop
     while (true) {
         menuLoop(); // Display options
-
         std::cout << "Choice: ";
-        if (!(std::cin >> userInput)) {
-            std::cout << "Invalid input. Input must be a menu option.\n";
+        std::getline(std::cin, line);
 
-            std::cin.clear();
+        try {
+            userInput = std::stoi(line);
+        }
+        catch(...) {
+            std::cout << "Invalid input.\n";
             continue;
         }
 
-        if (userInput == 1) { 
-            std::cout << "Enter your player's name: ";
+        if (userInput == 1) {
+            std::cout << "Enter your name: ";
+            std::getline(std::cin, line);
 
-            if (!(std::cin >> userName)) {
-                std::cout << "Invalid input. Input must be a valid name.";
-                continue;
-            }
+            player = new Player(line);
+        }
 
-            player = new Player(userName);
-         }
-        else if (userInput == 2) { std::cout << "\n2\n\n"; }
-        else if (userInput == 3) { std::cout << "\n3\n\n"; }
-        else if (userInput == 4) { std::cout << "\n4\n\n"; }
-        else if (userInput == 5) { break; }
+        if (userInput == 5) {
+            std::cout << "Quitting...\n";
+            break;
+        }
     }
 
     std::string name = player->getName();
